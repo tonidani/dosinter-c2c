6a61796f336169c322c1796113ee366f83b56679
tonidani <tonidani@gmail.com>
2023-05-22 21:30:04 +0200

    geo, camera testing

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -1,5 +1,5 @@
 # Mini Flask example app.
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return "Hello, world."
+
+@app.route('/camera', methods=['GET'])
+def get_camera():
+    return render_template('camera.html')
+
+@app.route('/geo', methods=['GET'])
+def get_geolocation():
+    return render_template('location.html')
+
 
 if __name__ == "__main__":
     app.run()
