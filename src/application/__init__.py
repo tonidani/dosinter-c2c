be84ce1f3c063187b87f69484fb26e4cbfa2fac2
tonidani <tonidani@gmail.com>
2023-05-29 01:01:36 +0200

    .

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -7,7 +7,17 @@ from functools import wraps
 import secrets
 import logging
 
-from utils import get_location
+import requests
+
+def get_location(ip_address):
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return location_data
 
 app = Flask(__name__)
 BASE_URL = "resp://scamapp/?data="
